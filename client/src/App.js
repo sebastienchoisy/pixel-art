a100faf47bafd591be306bda71ccdb2a8bd123e9
sebastienchoisy <58088513+sebastienchoisy@users.noreply.github.com>
2022-11-21 16:40:06 +0100

    feat(front) login + persistence connexion + form logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,38 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-  BrowserRouter,
-  Route, Routes,
+  Route, Routes, Navigate, useLocation,
 } from 'react-router-dom';
 import ScreenAccueil from './screens/Screen-accueil';
 import ScreenBoard from './screens/Screen-board';
 import ScreenProfil from './screens/Screen-profil';
 import ScreenLogin from './screens/Screen-login';
 import Header from './components/header/Header';
+import ScreenSignin from './screens/Screen-signin';
+import { getUserInfoWithCookie } from './services/APIService';
 
 function App() {
-  // const [data, setData] = React.useState(null);
-  //
-  // React.useEffect(() => {
-  //   fetch("/api")
-  //       .then((res) => res.json())
-  //       .then((data) => setData(data.message));
-  // }, []);
+  const location = useLocation();
+  const [user, setIsUser] = useState(undefined);
+
+  useEffect(() => {
+    const response = getUserInfoWithCookie();
+    if (response.success) {
+      setIsUser(getUserInfoWithCookie().user);
+    }
+  }, [location]);
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<ScreenAccueil />} />
-          <Route path="/boards" element={<ScreenBoard />} />
-          <Route path="/profil" element={<ScreenProfil />} />
-          <Route path="/login" element={<ScreenLogin />} />
-        </Routes>
-      </BrowserRouter>
+      <Header username={user ? user.username : undefined} />
+      <Routes>
+        <Route exact path="/" element={<ScreenAccueil />} />
+        <Route path="/boards" element={<ScreenBoard />} />
+        <Route path="/profil" element={user ? <ScreenProfil /> : <Navigate replace to="/" />} />
+        <Route path="/login" element={<ScreenLogin />} />
+        <Route path="/signin" element={!user ? <ScreenSignin /> : <Navigate replace to="/" />} />
+      </Routes>
     </div>
   );
 }
--- client/src/screens/Screen-signin.js
+++ client/src/screens/Screen-signin.js
@@ -3,19 +3,17 @@ import {
   Button, Form, FormGroup, Input, Label,
 } from 'reactstrap';
 import { useNavigate } from 'react-router-dom';
-import { login } from '../services/APIService';
+import { signin } from '../services/APIService';
 
-export default function ScreenLogin() {
+export default function ScreenSignin() {
   const [email, setEmail] = useState('');
   const [pwd, setPwd] = useState('');
-  const [hasCredentialsError, setHasCredentialsError] = useState(false);
+  const [checkPwd, setCheckPwd] = useState('');
   const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (login(email, pwd).success) {
+    if (signin(email, pwd).success) {
       navigate('/');
-    } else {
-      setHasCredentialsError(true);
     }
   };
 
@@ -23,41 +21,43 @@ export default function ScreenLogin() {
     <Form onSubmit={handleSubmit} className="form m-auto mt-5 login-form">
       <div className="text-start">
         <FormGroup>
-          <Label for="exampleEmail">
+          <Label for="email">
             Email
           </Label>
           <Input
-            id="exampleEmail"
+            id="email"
             name="email"
             placeholder="Email"
             type="email"
             onChange={(e) => setEmail(e.target.value)}
-            onKeyDown={() => setHasCredentialsError(false)}
           />
         </FormGroup>
         <FormGroup>
-          <Label for="examplePassword">
+          <Label for="pwd">
             Password
+          </Label>
+          <Input
+            id="pwd"
+            name="pwd"
+            placeholder="Mot de passe"
+            type="password"
+            onChange={(e) => setPwd(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="check-pwd">
+            Confirmer votre mot de passe
           </Label>
           <Input
             id="examplePassword"
             name="password"
-            placeholder="Password"
+            placeholder="Confirmer votre mot de passe"
             type="password"
-            onChange={(e) => setPwd(e.target.value)}
-            onKeyDown={() => setHasCredentialsError(false)}
+            onChange={(e) => setCheckPwd(e.target.value)}
           />
         </FormGroup>
-        {hasCredentialsError
-            && (
-              <div className="alert alert-danger alert-dismissible show" role="alert">
-                <button type="button" className="btn-close" aria-label="Close" onClick={() => setHasCredentialsError(false)} />
-                Erreur: Mauvais identifiants
-              </div>
-            )}
-
       </div>
-      <Button disabled={!(email && pwd)}>
+      <Button disabled={!(email && pwd && checkPwd)}>
         Submit
       </Button>
     </Form>
