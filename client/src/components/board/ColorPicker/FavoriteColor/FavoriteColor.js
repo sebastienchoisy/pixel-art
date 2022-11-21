453c32ab465a3b6ec13e52537b420a0996d55d73
NicolasParizet <72597303+NicolasParizet@users.noreply.github.com>
2022-11-21 15:59:03 +0100

    feat(front): color picker + favorite colors list

---
+++ client/src/components/board/ColorPicker/FavoriteColor/FavoriteColor.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './FavoriteColor.css';
+
+export default function FavoriteColor(props) {
+  const { color, onClick } = props;
+  return (
+    <div>
+      <button
+        type="button"
+        className="FavoriteColor my-1"
+        value={color}
+        onClick={onClick}
+        style={{ backgroundColor: color }}
+        alt="select a favorite color"
+      />
+    </div>
+  );
+}
+
+FavoriteColor.propTypes = {
+  color: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
